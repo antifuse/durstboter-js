290ca7b922e67c81a51450993a570979bdbd93c7
antifuse <dev@antifuse.eu>
2020-12-06 21:55:40 +0100

    was

--- src/log.ts
+++ src/log.ts
@@ -1,6 +1,6 @@
 import * as winston from "winston";
 export default winston.createLogger({
-    format: winston.format.combine(winston.format.timestamp({format:"YYYY-DD-MM HH:mm:ss"}), winston.format.printf(info=>`${info.timestamp} ${info.level} | ${info.message}`)),
+    format: winston.format.combine(winston.format.timestamp({format:"YYYY-MM-DD HH:mm:ss"}), winston.format.printf(info=>`${info.timestamp} ${info.level} | ${info.message}`)),
     transports: [
         new winston.transports.Console(),
         new winston.transports.File({filename: "durst.log"})
