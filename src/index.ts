1a374b0c3a587e2f74ef88654dc4ac466a6ac70c
antifuse <dev@antifuse.eu>
2020-12-06 21:38:50 +0100

    werden sie myxomyciert

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import Discord = require("discord.js");
 import fs = require("fs");
-import {Collection, DMChannel, Message, PermissionResolvable, TextBasedChannel, TextChannel, Channel} from "discord.js";
+import { Collection, DMChannel, Message, PermissionResolvable, TextBasedChannel, TextChannel, Channel } from "discord.js";
 import cron = require("node-cron");
 import * as winston from "winston";
 
@@ -9,10 +9,7 @@ import log from "./log";
 const client = new Discord.Client();
 const commands: Collection<string, Command> = new Discord.Collection();
 const commandFiles = fs.readdirSync(`./build/commands`).filter(file => file.endsWith('.js'));
-let config: any;
-fs.readFile("./config.json", {encoding: 'utf8'}, (err, data)=>{
-    if (!err) config = JSON.parse(data);
-});
+let config = JSON.parse(fs.readFileSync("./config.json", { encoding: 'utf8' }));
 log.info("Loaded config.");
 
 interface Command {
@@ -26,20 +23,20 @@ interface Command {
 
 for (const file of commandFiles) {
     const command: Command = require(`./commands/${file}`);
-    command.log = (message)=>log.info(message);
+    command.log = (message) => log.info(message);
     commands.set(command.name, command);
 }
 
 log.info(`Loaded ${commands.size} commands`);
 
 // Message on join
-client.once('ready',()=> {
+client.once('ready', () => {
     log.info('Ready.');
 });
 
 // command handler
 client.on('message', message => {
-    if(!message.content.startsWith(config.prefix) || message.author.bot) return;
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
     const args = message.content.slice(config.prefix.length).split(/ +/);
     const commandCall = args.shift().toLowerCase();
     const command: Command = commands.get(commandCall) || commands.find((cmd: Command) => cmd.aliases && cmd.aliases.includes(commandCall));
@@ -63,20 +60,22 @@ client.on('message', message => {
 // DM logger
 client.on('message', message => {
     // log message
-    log.info(`${message.author.tag}/${message.channel.id}: ${message.content}`);
-    if (!config.logoptout.includes(message.author.id)) fs.appendFile('messages.txt', `${message.author.tag}/${message.channel.id}: ${message.content}\n`,(err)=> {
-        if (err) log.error(err);
-    });
+    if (!config.logoptout.includes(message.author.id)) {
+        log.info(`${message.author.tag}/${message.channel.id}: ${message.content}`);
+        fs.appendFile('messages.txt', `${message.author.tag}/${message.channel.id}: ${message.content}\n`, (err) => {
+            if (err) log.error(err);
+        });
+    }
     // log dms to dmlog
     if (message.channel.type === 'dm') {
         client.channels.fetch(config.dmlog)
-            .then((channel: Channel) => {if (channel instanceof DMChannel) channel.send(`${message.author.tag} : ${message.content}`)});
+            .then((channel: Channel) => { if (channel instanceof DMChannel) channel.send(`${message.author.tag} : ${message.content}`) });
     }
 });
 
 let reactions: any;
 // Automatic reactions:
-fs.readFile("./reactions.json", {encoding: 'utf8'}, (err, data)=>{
+fs.readFile("./reactions.json", { encoding: 'utf8' }, (err, data) => {
     if (!err) reactions = JSON.parse(data);
 });
 log.info("Loaded reactions.")
@@ -90,21 +89,21 @@ client.on('message', message => {
 // Message quadrupling
 client.on('message', async message => {
     if (message.channel.type === 'text' && !message.author.bot) {
-        let last2 = (await message.channel.messages.fetch({before: message.id, limit: 2})).array();
+        let last2 = (await message.channel.messages.fetch({ before: message.id, limit: 2 })).array();
         if (!last2[0].author.bot && !last2[1].author.bot && message.content == last2[0].content && message.content == last2[1].content && !last2[0].author.equals(last2[1].author) && !message.author.equals(last2[0].author) && !message.author.equals(last2[1].author)) message.channel.send(message.content);
     }
 });
 
 // Watch reaction & config files
-fs.watch('./reactions.json',(event,name)=> {
-    fs.readFile("./reactions.json", {encoding: 'utf8'}, (err, data)=>{
+fs.watch('./reactions.json', (event, name) => {
+    fs.readFile("./reactions.json", { encoding: 'utf8' }, (err, data) => {
         if (!err) reactions = JSON.parse(data);
     });
     log.info("Reloaded reactions.")
 });
 
-fs.watch('./config.json',(event,name)=> {
-    fs.readFile("./config.json", {encoding: 'utf8'}, (err, data)=>{
+fs.watch('./config.json', (event, name) => {
+    fs.readFile("./config.json", { encoding: 'utf8' }, (err, data) => {
         if (!err) config = JSON.parse(data);
     });
     log.info("Reloaded config.")
@@ -113,9 +112,9 @@ fs.watch('./config.json',(event,name)=> {
 // hold my bot token, i'm going in!
 client.login(config.token);
 
-cron.schedule('0 20 * * *', ()=>{
-    config["broadcast-channels"].forEach((id: string)=>{
-        client.channels.fetch(id).then((channel)=>{
+cron.schedule('0 20 * * *', () => {
+    config["broadcast-channels"].forEach((id: string) => {
+        client.channels.fetch(id).then((channel) => {
             if (channel instanceof TextChannel || channel instanceof DMChannel) channel.send('TAGESSCHAU O\'CLOCK!');
         })
     })
